210885467d06c48e77a28415493628aa10f5543f
jiangzhi.xie <xiejiangzhi@gmail.com>
2019-03-18 22:50:19 +0800

    MISC - Remove invalid code and add bin/dev & bin/prod

--- app.rb
+++ app.rb
@@ -2,42 +2,10 @@ $app_name = 'XjzProxy'
 
 module Xjz
   require File.expand_path('../env', __FILE__)
-  extend ActiveSupport
 
   # init sub module
   module Reslover; end
   class ProxyClient; end
 
-  Dir[File.join($root, 'src/xjz/**/*.rb')].each { |path| load path }
-
-  # %w{
-  #   CertManager
-
-  #   IOHelper
-  #   Logger
-  #   Tracker
-
-  #   ProxyServer
-  #   MyPumaServer
-  #   WriterIO
-  #   ViewEntity
-
-  #   Request
-  #   Response
-
-  #   RequestDispatcher
-  # }.each do |name|
-  #   autoload name, "xjz/#{name.underscore}"
-  # end
-
-  # module Reslover
-  #   %w{
-  #     SSL
-  #     HTTP1
-  #     HTTP2
-  #     WebUI
-  #   }.each do |name|
-  #     autoload name, "xjz/reslover/#{name.underscore}"
-  #   end
-  # end
+  Dir[File.join($root, 'src/xjz/**/*.rb')].sort.each { |path| load path }
 end
